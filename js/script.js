e7a5247ec25eee682b1555f6becc1824c98737aa
Ryham <ryham.rabhi24@ump.ac.ma>
2026-01-06 14:10:26 +0100

    Initialisation du projet web

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+    animateDisplay();
+}
+
+function clearDisplay() {
+    display.value = "";
+    animateDisplay();
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+        animateDisplay();
+    } catch {
+        display.value = "Erreur";
+        setTimeout(() => display.value = "", 1000);
+    }
+}
+
+/* Animation écran */
+function animateDisplay() {
+    display.style.transform = "scale(1.05)";
+    setTimeout(() => {
+        display.style.transform = "scale(1)";
+    }, 100);
+}
